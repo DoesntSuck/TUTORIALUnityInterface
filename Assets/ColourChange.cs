c9044d074ed848ba99827228c9b3390d92c09d85
Stephen <clarksj4@student.op.ac.nz>
2016-08-30 18:17:37 +1200

    Initial commit

---
+++ Assets/ColourChange.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColourChange : MonoBehaviour
+{
+    // Public variables will be exposed in the inspector
+
+    // Colours to change from and to
+    public Color From;
+    public Color To;
+
+    // The duration of the colour change
+    public float duration;
+
+    // The current time the colour change has been occuring for
+    float time;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // t is the amount to interpolate by, i.e. the amount of colour 1 and 2 to mix. 0 = all colour1, 1 = all colour2, 0.5 = 50 : 50 mix
+        float t = time / duration;
+
+        // The current colour (Lerp = linear interpolate)
+        Color current = Color.Lerp(From, To, t);
+
+        // Get the mesh renderer component attached to THIS game object
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+        // Get the material the renderer is using to draw the mesh and set its colour to the one we calculated
+        renderer.material.color = current;
+
+        // Keep track of the time
+        time += Time.deltaTime;
+
+        // Reset time when duration is exceeded so that lerp starts again
+        if (time > duration) time = 0;
+	}
+}
---
+++ Assets/Rotating.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotating : MonoBehaviour
+{
+    // The degrees per second that the object will rotate
+    public float speed = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // The amount to rotate the object this frame. Multiplying by Time.delta time makes it a per second change
+        float degrees = speed * Time.deltaTime;
+
+        // Rotate the object around the world upwards vector (x: 0, y: 1, z: 0)
+        transform.Rotate(Vector3.up, degrees, Space.World);	
+	}
+}
